4b92545778777e981ffeb42e17e3511ef7c8a879
Orion Qin <xiaolei_qin@outlook.com>
2020-06-17 16:36:46 -0400

    Upload the Porject.

---
+++ Assets/Detect_Emotions.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/**
+ * @author Orion Qin
+ */
+public class Detect_Emotions : MonoBehaviour
+{
+    public ParticleSystem mode;
+    public int state;
+    private int mood;
+    private int lastState;
+    private int newState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        if (state == 0)
+            mode.Play();
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Obtain last state
+        lastState = mood;
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            // Switch to happy state
+            mood = 1;
+            //Notify state change
+            newState = mood;
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            // Switch to sad state
+            mood = 2;
+            //Notify state change
+            newState = mood;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Back t default state
+            mood = 0;
+            //Notify state change
+            newState = mood;
+        }
+
+        if (lastState != newState && mood == state)
+        {
+            mode.Play();
+
+        }
+        if (mood != state)
+        {
+            mode.Stop();
+        }
+        
+
+    }
+
+
+}
---
+++ Assets/ProximityParticle.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * @author Orion Qin
+ */
+
+public class ProximityParticle : MonoBehaviour
+{
+    private GameObject player;
+    public GameObject parentObj;
+    public ParticleSystem happyMode;
+    public ParticleSystem sadMode;
+    public ParticleSystem defaultMode;
+    private int state;
+    private int lastState;
+    private int newState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        defaultMode.Play();
+        state = 0;
+        lastState = -1;
+        newState = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            // Switch to happy state
+            state = 1;
+            //Notify state change
+            newState = state;
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            // Switch to sad state
+            state = 2;
+            //Notify state change
+            newState = state;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Back t default state
+            state = 0;
+            //Notify state change
+            newState = state;
+        }
+
+        if (Vector3.Distance(player.transform.position, parentObj.transform.position) < 7)
+        {
+
+            //Happy Mode
+            if (state == 1 && lastState != newState)
+            {
+                happyMode.Play();
+                defaultMode.Stop();
+                sadMode.Stop();
+
+                //Obtain the last state
+                lastState = newState;
+
+            } else
+
+            //Sad Mode
+            if (state == 2 && lastState != newState)
+            {
+                happyMode.Stop();
+                defaultMode.Stop();
+                sadMode.Play();
+
+                //Obtain the last state
+                lastState = newState;
+
+            } else 
+
+            //Default Mode
+            if (state == 0 && lastState != newState)
+            {
+                happyMode.Stop();
+                defaultMode.Play();
+                sadMode.Stop();
+
+                //Obtain the last state
+                lastState = newState;
+            }
+
+        }
+
+
+    }
+}
